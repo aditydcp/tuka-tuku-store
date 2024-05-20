b7a2ac04f3acb77ddc267e103acddd5e385a5d93
Aditya Cahyo <aditydcp@gmail.com>
2024-05-20 15:51:17 +0700

    update products endpoints

--- src/app/api/products/[product_id]/route.ts
+++ src/app/api/products/[product_id]/route.ts
@@ -1,9 +1,175 @@
-import { NextResponse } from "next/server";
+import { prisma } from "@/utils/configs/db";
+import { nullIfError } from "@/utils/functions";
+import { productSchema } from "@/utils/types/products";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-    return NextResponse.json({message: "success", method: "GET", data: []})
+interface Params {
+    params: { product_id: string }
 }
 
-export async function POST() {
-    return NextResponse.json({message: "success", method: "POST", data: []})
+interface DataToUpdate {
+    name: string;
+    description: string;
+    price: string;
+    category_id: number;
+    image?: string;
+}
+
+export async function GET(request: NextRequest, context: Params) {
+    try {
+        const { product_id } = context.params
+
+        const data = await prisma.product.findFirst({
+            where: {
+                id: product_id,
+            },
+            include: {
+                category: {
+                    select: { name: true },
+                },
+            },
+            cacheStrategy: { ttl: 60 },
+        });
+
+        if (!data) {
+            return NextResponse.json({
+                message: "Failed to get product, data not found",
+                method: "GET",
+                data,
+                error: "The data you are trying to retrieve does not exist"
+            }, { status: 404 })
+        }
+
+        return NextResponse.json({
+            message: "Successfully get product",
+            method: "GET",
+            data,
+            error: null,
+        })
+    } catch (error) {
+        console.error(error)
+
+        return NextResponse.json({
+            message: "An error has occurred, please try again later.",
+            method: "GET",
+            data: null,
+            error: (error as Error).message,
+        }, { status: 500 })
+    }
+}
+
+export async function PUT(request: NextRequest, context: Params) {
+    try {
+        // TODO: Protect this endpoint
+
+        const formData = await request.formData()
+
+        const name = formData.get("name") as string
+        const description = formData.get("description") as string
+        const price = formData.get("price") as string
+        const image = formData.get("image") as File
+        const category_id = formData.get("category_id") as string
+
+        const validatedFields = productSchema.safeParse({
+            name,
+            description,
+            price,
+            image: image ?? undefined,
+            category_id
+        })
+
+        if (!validatedFields.success) {
+            return NextResponse.json({
+                message: "Failed to create product, please check your input",
+                method: "POST",
+                data: null,
+                error: validatedFields.error.flatten().fieldErrors
+            },
+                { status: 400 }
+            )
+        }
+
+        const { product_id } = context.params
+
+        let dataToUpdate: DataToUpdate = {
+            name,
+            description,
+            price,
+            category_id: +category_id!,
+        };
+
+        // TODO: Upload image to cloudinary
+        dataToUpdate.image = undefined;
+
+        const data = await nullIfError(prisma.product.update)({
+            where: {
+                id: product_id,
+            },
+            data: dataToUpdate,
+        });
+
+        if (!data) {
+            return NextResponse.json({
+                message: "Failed to update product, data not found",
+                method: "PUT",
+                data,
+                error: "The data you are trying to update does not exist"
+            }, { status: 404 })
+        }
+
+        return NextResponse.json({
+            message: "Successfully update product",
+            method: "PUT",
+            data,
+            error: null,
+        })
+    } catch (error) {
+        console.error(error)
+
+        return NextResponse.json({
+            message: "An error has occurred, please try again later",
+            method: "PUT",
+            data: null,
+            error: (error as Error).message,
+        }, { status: 500 })
+    }
+}
+
+export async function DELETE(request: NextRequest, context: Params) {
+    try {
+        // TODO: Protect this endpoint
+
+        const { product_id } = context.params
+
+        const data = await nullIfError(prisma.category.delete)({
+            where: {
+                id: Number(product_id),
+            },
+        })
+
+        if (!data) {
+            return NextResponse.json({
+                message: "Failed to delete product, data not found",
+                method: "DELETE",
+                data,
+                error: "The data you are trying to delete does not exist"
+            }, { status: 404 })
+        }
+
+        return NextResponse.json({
+            message: "Successfully delete product",
+            method: "DELETE",
+            data,
+            error: null,
+        })
+    } catch (error) {
+        console.error(error)
+
+        return NextResponse.json({
+            message: "An error has occurred, please try again later.",
+            method: "DELETE",
+            data: null,
+            error: (error as Error).message,
+        }, { status: 500 })
+    }
 }
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,20 +1,53 @@
 import { NextResponse, NextRequest } from "next/server";
 import { productSchema } from "@/utils/types/products";
+import { prisma } from "@/utils/configs/db";
 
 export async function GET(request: NextRequest) {
-    // TODO: Modularize query params handler
-    const searchParams = request.nextUrl.searchParams
+    try {
+        const data = await prisma.product.findMany({
+            include: {
+                category: {
+                    select: {
+                        name: true,
+                    },
+                },
+            },
+            orderBy: {
+                created_at: "desc",
+            },
+            cacheStrategy: { ttl: 60 },
+        });
 
-    console.log(searchParams)
-    return NextResponse.json({message: "success", method: "GET", data: []})
+        const totalCount = await prisma.product.count();
+        const totalPages = Math.ceil(totalCount / 10);
+
+        return NextResponse.json({
+            message: "Successfully get products",
+            metadata: {
+                totalCount,
+                totalPages,
+            },
+            data,
+        });
+    } catch (error) {
+        console.log(error);
+
+        return NextResponse.json(
+            {
+                message: "Get products failed, please try again later",
+                reason: (error as Error).message,
+            },
+            { status: 500 }
+        );
+    }
 }
 
 export async function POST(request: NextRequest) {
-    // TODO: Protect this method
-
     try {
+        // TODO: Protect this endpoint
+
         const formData = await request.formData()
-        
+
         const name = formData.get("name") as string
         const description = formData.get("description") as string
         const price = formData.get("price") as string
@@ -31,29 +64,40 @@ export async function POST(request: NextRequest) {
 
         if (!validatedFields.success) {
             return NextResponse.json({
-                message: "Failed to create product. Please check your input.",
+                message: "Failed to create product, please check your input",
                 method: "POST",
                 data: null,
                 error: validatedFields.error.flatten().fieldErrors
             },
-            { status: 400 }
-        )}
+                { status: 400 }
+            )
+        }
 
         // TODO: Upload image to cloudinary
+        let imageUrl = null
 
-        // TODO: Create new record in database
+        const data = await prisma.product.create({
+            data: {
+                name,
+                description,
+                price,
+                image: imageUrl,
+                category_id: +category_id!,
+            },
+        });
 
         return NextResponse.json({
-            message: "Product successfully created.",
+            message: "Product successfully created",
             method: "POST",
-            data: [],
+            data,
+            error: null,
         }, { status: 201 })
     }
     catch (error) {
         console.error(error)
 
         return NextResponse.json({
-            message: "An error has occurred. Please try again later.", 
+            message: "An error has occurred please try again later",
             method: "POST",
             data: null,
             error: (error as Error).message,
