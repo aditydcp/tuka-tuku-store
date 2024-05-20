58b3056c9de7f1552ef9eee965ddb0cc64d081d8
Aditya Cahyo <aditydcp@gmail.com>
2024-05-21 02:41:47 +0700

    setup file upload

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,20 @@
+import { rejects } from "assert"
+import { Session } from "next-auth"
+import { cloudinaryConfig } from "./configs/storage"
+
 type AnyFunction = (...args: any[]) => any
 
-export const nullIfError = <Func extends AnyFunction>(func: Func) => 
+type Options = {
+  tags?: string[]
+  folder: string
+}
+
+type UploadPromise = {
+  message: string
+  data: string
+}
+
+export const nullIfError = <Func extends AnyFunction>(func: Func) =>
   async (...args: Parameters<Func>) => {
     try {
       return await func(...args)
@@ -21,4 +35,36 @@ export const isNotLoggedIn = (auth: Session | null, isAdmin: boolean = false) =>
   } else {
     return true
   }
+}
+
+export const fileUploader = async (
+  image: File, 
+  { tags, folder }: Options
+) => {
+  const arrayBuffer = await image.arrayBuffer()
+  const buffer = new Uint8Array(arrayBuffer)
+
+  return new Promise<UploadPromise>((resolve, reject) => {
+    cloudinaryConfig.uploader.upload_stream(
+      {
+        tags,
+        resource_type: "auto",
+        folder
+      },
+      function (error, result) {
+        if (error) {
+          reject({
+            message: error.message,
+            data: null,
+          })
+          return
+        }
+        resolve({
+          message: "success",
+          data: result?.secure_url ?? "",
+        })
+      }
+    )
+    .end(buffer)
+  })
 }
